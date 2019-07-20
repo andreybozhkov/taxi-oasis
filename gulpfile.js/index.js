406ecaf88a5943d4fce71388e49c640e7ebb6914
andreybozhkov <bozhkov.andrey@gmail.com>
2019-07-20 20:16:47 +0300

    Revert "Added gulp 1st test task"
    This reverts commit 959068ee0e1cfc33cbac26d507f0489c99285e82.

--- gulpfile.js/index.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const devBuild = (process.env.NODE_ENV !== 'production');
-const src = '../src/';
-const build = '../build/';
-
-exports.default = (cb) => {
-    console.log('Gulp works!');
-    cb();
-}
